2d2e0edfd3d26666113b354825929c008acbbc4b
Ivan Peralta <Ivperalta112@gmail.com>
2025-10-20 16:19:51 -0700

    Comiting Files after branch

--- main1.java
+++ main1.java
@@ -1,10 +1,14 @@
 class main1{
     public static void main(String[] args){
         System.out.println("Is my mic on?");
+
+        System.out.println("UUUUUUUUUUUU");
     }
 
 
     public static String evilLaugh(){
         return System.out.println("HAHAHAHAHA");
     }
+
+    //MEGA CHANGE SIRS
 }
