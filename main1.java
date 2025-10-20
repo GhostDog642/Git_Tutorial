c6e58ab31eaeb527300e7cbda1df27aa3920fcea
Ivan Peralta <Ivperalta112@gmail.com>
2025-10-20 16:09:14 -0700

    Second Commit with updated file

--- main1.java
+++ main1.java
@@ -2,4 +2,9 @@ class main1{
     public static void main(String[] args){
         System.out.println("Is my mic on?");
     }
+
+
+    public static String evilLaugh(){
+        return System.out.println("HAHAHAHAHA");
+    }
 }
